2d89e6e089dc16a2b41b47826e45e73d5dc84443
amsharidwan <ridwanamshari0310@gmail.com>
2023-06-29 08:17:54 +0700

    Variables and basic data types

---
+++ helloworld.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int main(){
+    
+    // integers (whole number)
+    int age = 21;
+    int year = 2023;
+    int days = 7;
+
+    // double (number including decimal)
+    double price = 10.99;
+    double gpa = 2.5;
+    double temperature = 25.1;
+
+    // single character
+    char grade = 'A';
+    char initial = 'C';
+    char dollarSign = '$';
+
+    // boolean (true or false)
+    bool student = false;
+    bool power = true;
+    bool forSale = true;
+
+    // string (objects that represents a sequence of text)
+    std::string name = "Bro";
+    std::string day = "Friday";
+    std::string food = "pizza";
+
+    std::cout << "Hello " << name << '\n';
+    std::cout << "You are " << age << " years old";
+
+    return 0;
+}
