06c2147a9550dd22a7f24a77a2f34232faf02188
amsharidwan <ridwanamshari0310@gmail.com>
2023-06-29 08:35:29 +0700

    Const

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,34 +1,13 @@
 #include <iostream>
 
-int main(){
-    
-    // integers (whole number)
-    int age = 21;
-    int year = 2023;
-    int days = 7;
-
-    // double (number including decimal)
-    double price = 10.99;
-    double gpa = 2.5;
-    double temperature = 25.1;
-
-    // single character
-    char grade = 'A';
-    char initial = 'C';
-    char dollarSign = '$';
-
-    // boolean (true or false)
-    bool student = false;
-    bool power = true;
-    bool forSale = true;
-
-    // string (objects that represents a sequence of text)
-    std::string name = "Bro";
-    std::string day = "Friday";
-    std::string food = "pizza";
-
-    std::cout << "Hello " << name << '\n';
-    std::cout << "You are " << age << " years old";
+int main() {
+    // The const keyword specifies that a variable's value is constant
+    // tells the compiler to prevent anything from modifying it
+    // (read-only)
+    const double PI = 3.14159;
+    const int LIGHT_SPEED = 299792458;
+    const int WIDTH = 1920;
+    const int HEIGHT = 1080;
 
     return 0;
 }
