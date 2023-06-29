bd1e9a7e7c3ccabbc5458d6d1f248afc37bba4ce
amsharidwan <ridwanamshari0310@gmail.com>
2023-06-29 15:11:12 +0700

    Useful math related functions

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,27 +1,24 @@
 #include <iostream>
-
-// cout << (insertion operator)
-// cin >> (extraction operator)
+#include <cmath>
 
 int main() 
 {
-    std::string name;
-    int age;
+    // double x = 3;
+    // double x = 3.14;
+    double x = 3.99;
+    double y = 4;
+    double z;
 
-    // std::cout << "What's your name?: ";
-    // std::cin >> name;
-    
-    // std::cout << "What's your full name?: ";
-    // std::getline(std::cin, name);
+    // z = std::max(x, y);
+    // z = std::min(x, y);
+    // z = pow(2, 4);
+    // z = sqrt(9);
+    // z = abs(-3);
+    // z = round(x);
+    // z = ceil(x);
+    z = floor(x);
 
-    std::cout << "What's your age?: ";
-    std::cin >> age;
-
-    std::cout << "What's your full name?: ";
-    std::getline(std::cin >> std::ws, name);
-
-    std::cout << "Hello " << name << '\n';
-    std::cout << "You are " << age << " years old";
+    std::cout << z;
 
     return 0;
 }
