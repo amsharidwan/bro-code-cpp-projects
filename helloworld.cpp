ad2d098ae3dd07ee56ac9b2008e5e3ce1ed054f0
amsharidwan <ridwanamshari0310@gmail.com>
2023-07-01 21:37:54 +0700

    While loops

--- helloworld.cpp
+++ helloworld.cpp
@@ -4,42 +4,17 @@ int main() {
 
     std::string name;
 
-    std::cout << "Enter your name: ";
-    std::getline(std::cin, name);
-
-    // if(name.length() > 12){
-    //     std::cout << "Your name can't be over 12 character" << name.length();
-    // }
-    // else{
-    //     std::cout << "Welcome " << name;
-    // }
-
     // if(name.empty()){
-    //     std::cout << "You didn't enter your name";
-    // }
-    // else{
-    //     std::cout << "Hello " << name;
+    //     std::cout << "Enter your name: ";
+    //     std::getline(std::cin, name);
     // }
 
-    // name.clear();
+    while(name.empty()){
+        std::cout << "Enter your name: ";
+        std::getline(std::cin, name);
+    }
 
-    // std::cout << "Hello " << name;
-
-    // name.append("@gmail.com");
-
-    // std::cout << "Your username is now " << name;
-
-    // std::cout << name.at(0);
-
-    // name.insert(0, "@");
-
-    // std::cout << name;
-
-    // std::cout << name.find(' ');
-
-    name.erase(0, 3);
-
-    std::cout << name;
+    std::cout << "Hello " << name;
 
     return 0;
 }
