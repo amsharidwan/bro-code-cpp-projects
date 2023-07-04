dcf8d502d0fc490859d2f60f3e0c71f0a5d4779a
amsharidwan <ridwanamshari0310@gmail.com>
2023-07-05 06:41:29 +0700

    Random number generator

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,36 +1,19 @@
 #include <iostream>
+#include <ctime>
 
 int main() {
 
-    /*
-        loop(){
-            loop(){
+    // pseudo-random = NOT truly random (but close)
 
-            }
-        }
-    */
+    srand(time(NULL));
 
-    int rows;
-    int columns;
-    char symbol;
+    int num1 = (rand() % 6) + 1;
+    int num2 = (rand() % 6) + 1;
+    int num3 = (rand() % 6) + 1;
 
-    std::cout << "How many rows?: ";
-    std::cin >> rows;
-
-    std::cout << "How many columns: ";
-    std::cin >> columns;
-
-    std::cout << "Enter a symbol to use: ";
-    std::cin >> symbol;
-
-    for (int i = 1; i <= rows; i++)
-    {
-        for (int j = 1; j <= columns; j++)
-        {
-            std::cout << symbol;
-        }
-        std::cout << '\n';
-    }
+    std::cout << num1 << '\n';
+    std::cout << num2 << '\n';
+    std::cout << num3 << '\n';
 
     return 0;
 }
