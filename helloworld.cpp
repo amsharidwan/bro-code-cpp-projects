6f24391e852ffa6b9eed85852201f8ff0f673120
amsharidwan <ridwanamshari0310@gmail.com>
2023-07-05 06:23:54 +0700

    Nested loops

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,16 +2,35 @@
 
 int main() {
 
-    // break = break out of a loop
-    // continue = skip current iteration
+    /*
+        loop(){
+            loop(){
 
-    for (int i = 1; i <= 20; i++){
-        if(i == 13){
-            // break;
-            continue;
+            }
         }
-        std::cout << i << '\n';
+    */
+
+    int rows;
+    int columns;
+    char symbol;
+
+    std::cout << "How many rows?: ";
+    std::cin >> rows;
+
+    std::cout << "How many columns: ";
+    std::cin >> columns;
+
+    std::cout << "Enter a symbol to use: ";
+    std::cin >> symbol;
+
+    for (int i = 1; i <= rows; i++)
+    {
+        for (int j = 1; j <= columns; j++)
+        {
+            std::cout << symbol;
+        }
+        std::cout << '\n';
     }
 
-        return 0;
+    return 0;
 }
