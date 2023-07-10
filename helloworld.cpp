c0e6a52651d697367d460459de12a0481bc2e027
amsharidwan <ridwanamshari0310@gmail.com>
2023-07-10 20:13:47 +0700

    Return keyword

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,23 +1,39 @@
 #include <iostream>
 
-void happyBirthday(std::string name, int age);
+// double square(double length);
+// double cube(double length);
+
+std::string concatStrings(std::string string1, std::string string2);
 
 int main() {
 
-    // function = a block of reusable code
+    // return = return a value back to the spot
+    //          where you called the encompassing function
 
-    std::string name = "Ridwan";
-    int age = 24;
+    // double length = 6.0;
+    // double area = square(length);
+    // double volume = cube(length);
 
-    happyBirthday(name, age);
+    // std::cout << "Area: " << area << "cm^2\n";
+    // std::cout << "Volume: " << volume << "cm^3\n";
+
+    std::string firstName = "Ridwan";
+    std::string lastName = "Amshari";
+    std::string fullName = concatStrings(firstName, lastName);
+
+    std::cout << "Hello " << fullName;
 
     return 0;
 }
 
-void happyBirthday(std::string name, int age){
-    std::cout << "Happy Birthday to " << name << '\n';
-    std::cout << "Happy Birthday to " << name << '\n';
-    std::cout << "Happy Birthday dear " << name << '\n';
-    std::cout << "Happy Birthday to " << name << '\n';
-    std::cout << "You are " << age << " years old\n";
+// double square(double length){
+//     return length * length;
+// }
+
+// double cube(double length){
+//     return length * length * length;
+// }
+
+std::string concatStrings(std::string string1, std::string string2){
+    return string1 + " " + string2;
 }
